39c8fb39c244fabee2f2fe4c83e9d5adc1416012
entsarahmed <entesara500@gmail.com>
2025-04-24 23:02:50 +0200

    Add Get By Id

--- SurveyBasket.Api/Controllers/PollsController.cs
+++ SurveyBasket.Api/Controllers/PollsController.cs
@@ -3,12 +3,26 @@
 [ApiController]
 public class PollsController : ControllerBase
 {
-    private readonly List<Poll> _polls = [];
-    [HttpGet]
-    [Route("")]
+    private readonly List<Poll> _polls = [
+        new Poll{
+            Id = 1,
+            Title = "Poll 1",
+            Description = "My first poll"
+        }
+        ];
+
+    
+    [HttpGet("")]
     public IActionResult GetAll()
     {
         return Ok(_polls);
     }
-  
+    [HttpGet("{Id}")]
+    public IActionResult Get(int id)
+    {
+        var poll = _polls.SingleOrDefault(x => x.Id == id);
+
+        return poll is null ? NotFound() : Ok(poll);
+    }
+
 }

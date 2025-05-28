fe1244322c6fb500d16c76a0edb10a1a5c67f5d5
entsarahmed <entesara500@gmail.com>
2025-05-28 14:12:06 +0300

    Add Get Refresh Token Method

--- SurveyBasket.Api/Services/AuthService.cs
+++ SurveyBasket.Api/Services/AuthService.cs
@@ -37,6 +37,33 @@ public class AuthService
         //return new AuthResponse()
         return new AuthResponse(user.Id, user.Email, user.FirstName, user.LastName,token,expiresIn, refreshToken, refreshTokenExpiration);
     }
+    public async Task<AuthResponse?> GetRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default)
+    {
+       var userId = _jwtProvider.ValidateToken(token);
+        if (userId is null)
+            return null;
+        var user = await _userManager.FindByIdAsync(userId);
+        if(user is null) 
+            return null;
+        var userRefreshToken = user.RefreshTokens.SingleOrDefault(x => x.Token == refreshToken && x.IsActive);
+        if (userRefreshToken is null) 
+            return null;
+        userRefreshToken.RevokedOn = DateTime.UtcNow;
+
+        var (newToken, expiresIn) = _jwtProvider.GenerateToken(user);
+        var newRefreshToken = GenerateRefreshToken();
+        var refreshTokenExpiration = DateTime.UtcNow.AddDays(_refreshTokenExpiryDays);
+
+        user.RefreshTokens.Add(new RefreshToken
+        {
+            Token = newRefreshToken,
+            ExpiresOn = refreshTokenExpiration
+        });
+        await _userManager.UpdateAsync(user);
+        //return new AuthResponse()
+        return new AuthResponse(user.Id, user.Email, user.FirstName, user.LastName, token, expiresIn, newRefreshToken, refreshTokenExpiration);
+
+    }
     private static string GenerateRefreshToken()
     {
         return Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
--- SurveyBasket.Api/Services/IAuthService.cs
+++ SurveyBasket.Api/Services/IAuthService.cs
@@ -5,4 +5,6 @@ namespace SurveyBasket.Api.Services;
 public interface IAuthService
 {
     Task<AuthResponse?> GetTokenAsync(string email, string password,CancellationToken cancellationToken = default);
+    Task<AuthResponse?> GetRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default);
+
 }

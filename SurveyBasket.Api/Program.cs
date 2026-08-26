f026498de2c55fd0d8a909f2b0010c00c0720fc5
entsarahmed <Doctor@DESKTOP-HMG0CU3>
2026-08-26 22:16:19 +0300

    add changes

--- SurveyBasket.Api/Program.cs
+++ SurveyBasket.Api/Program.cs
@@ -6,21 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDependencies(builder.Configuration);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-     app.MapOpenApi();
-   //  app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json","v1"));
-     app.MapScalarApiReference();
+    //  app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();//(options => options.SwaggerEndpoint("/openapi/v1.json","v1"));
+    // app.MapScalarApiReference();
     
 
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
